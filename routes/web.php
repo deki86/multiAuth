8cc3c7f72a8a3441534b194104554761367712d2
Dejan Milosev <deja86@gmail.com>
2018-01-16 00:14:43 +0100

    Create Admin controller and routes for admin users

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +18,22 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')->group(function () {
+    // Admin registrtion and login/logout routes
+    Route::get('/', 'AdminController@index')->name('admin.home');
+    Route::get('/login', 'Admin\LoginController@showLoginForm')->name('admin.login');
+    Route::post('/login', 'Admin\LoginController@login')->name('admin.login.submit');
+
+    Route::post('/logout', 'Admin\LoginController@logout')->name('admin.logout');
+
+    Route::get('/register', 'Admin\RegisterController@showRegistrationForm')->name('admin.register');
+    Route::post('/register', 'Admin\RegisterController@register')->name('admin.register');
+
+    Route::post('/password/email', 'Admin\ForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
+    Route::get('/password/reset', 'Admin\ForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
+
+    Route::post('/password/reset', 'Admin\ResetPasswordController@reset');
+    Route::get('/password/reset/{token}', 'Admin\ResetPasswordController@showResetForm')->name('admin.password.reset');
+
+});
